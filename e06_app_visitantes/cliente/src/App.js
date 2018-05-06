2b166b1464c273c10044399945303924bcba964f
lucas <lucasjoao.lj@gmail.com>
2018-05-06 17:50:23 -0300

    start app.js final

--- e06_app_visitantes/cliente/src/App.js
+++ e06_app_visitantes/cliente/src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Panel} from 'primereact/components/panel/Panel'
+import {Button} from 'primereact/components/button/Button'
+import { Chart } from 'primereact/components/chart/Chart'
+
+import 'primereact/resources/primereact.min.css'
+import 'primereact/resources/themes/omega/theme.css'
+import 'font-awesome/css/font-awesome.css'
+
+// imports and obtenhaDados from step by step
 
 class App extends Component {
+  state = {
+    meses: undefined,
+    visitantes: undefined,
+    exibindo: false
+  }
+
+  obtenhaDados() {
+    fetch('/dados')
+      .then(r => Promise.resolve(r))
+      .then(r => r.json())
+      .then(({meses, visitantes}) => {
+        this.setState({meses, visitantes, exibindo: true})
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Panel>
+        Número de visitantes no site.
+      </Panel>
     );
   }
 }
