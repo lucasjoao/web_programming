8dcf0429a84eaddd4637269475ba81fc0ea57bb7
lucas <lucasjoao.lj@gmail.com>
2018-05-06 16:38:53 -0300

    server side ok

--- e06_app_visitantes/servidor/src/app.js
+++ e06_app_visitantes/servidor/src/app.js
@@ -1,1 +1,13 @@
-console.log('Working! :)')
+import express from 'express';
+
+const app = express();
+
+app.get("/dados", (req, res) => {
+  const dados = {
+    meses: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
+    visistantes: [23, 32, 12, 28, 36, 19]
+  }
+  res.json(dados);
+});
+
+app.listen(3001, () => console.log('No ar, porta 3001'));
